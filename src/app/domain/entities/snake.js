b6ed218590567619c702593f58ebff153439d66a
Pedro Luís <pedroluis@MacBook-Air-de-Pedro-5.local>
2022-08-15 20:53:36 +0200

    feat: add snake features

--- src/app/domain/entities/snake.js
+++ src/app/domain/entities/snake.js
@@ -1,18 +1,55 @@
-export default function buildMakeSnake() {
-  return function makeSnake({ size, headStartingPosition }) {
+export default function buildMakeSnake(makeNode) {
+  return function makeSnake({ initialSize, initialHeadPosition }) {
     let directionX = 0, directionY = 0
+    let nodes = []
 
-    validatePositionObject(headStartingPosition)
+    validatePositionObject(initialHeadPosition)
+    initialize()
 
     function validatePositionObject(position) {
       if (!Object.keys(position).includes('x') || !Object.keys(position).includes('y'))
         throw new Error('Position object must have an x and a y')
     }
 
+    function initialize() {
+      for (let i = 0; i < initialSize; i++) {
+        const [x, y] = [initialHeadPosition.x - i, initialHeadPosition.y]
+        nodes.push(makeNode({ x, y }))
+      }
+    }
+
     return Object.freeze({
-      getSize: () => size,
-      grow: () => size++,
-      getDirection: () => ({ x: directionX, y: directionY})
+      getSize: () => nodes.length,
+      grow: () => {
+        const tail = nodes[nodes.length - 1]
+        nodes.push({ ...tail })
+      },
+      getDirection: () => ({ x: directionX, y: directionY}),
+      getNodes: () => nodes,
+      lookUp: () => {
+        if (directionY == 1) return
+
+        directionX = 0
+        directionY = -1
+      },
+      lookDown: () => {
+        if (directionY == -1) return
+
+        directionX = 0
+        directionY = 1
+      },
+      lookLeft: () => {
+        if (directionX == 1) return
+        
+        directionY = 0
+        directionX = -1
+      },
+      lookRight: () => {
+        if (directionX == -1) return
+        
+        directionY = 0
+        directionX = 1
+      }
     })
   }
 }
--- src/app/domain/entities/snake.spec.js
+++ src/app/domain/entities/snake.spec.js
@@ -1,32 +1,122 @@
-import buildMakeSnake from './snake';
+import buildMakeSnake from './snake'
+import buildMakeNode from './node'
 
 describe('Snake', () => {
-  const makeSnake = buildMakeSnake()
+  const makeNode = buildMakeNode()
+  const makeSnake = buildMakeSnake(makeNode)
 
   it('should create a snake', () => {
-    const sut = makeSnake({ size: 1, headStartingPosition: { x: 2, y: 3 }})
+    const sut = makeSnake({ initialSize: 1, initialHeadPosition: { x: 2, y: 3 }})
 
     expect(sut.getSize()).toBe(1)
   });
 
-  it('should not create a snake with an invalid headStartingPosition - no x', () => {
-    expect(() => makeSnake({ size: 1, headStartingPosition: { s: 2, y: 3 }})).toThrow()
+  it('should not create a snake with an invalid initialHeadPosition - no x', () => {
+    expect(() => makeSnake({ initialSize: 1, initialHeadPosition: { s: 2, y: 3 }})).toThrow()
   });
 
-  it('should not create a snake with an invalid headStartingPosition - no y', () => {
-    expect(() => makeSnake({ size: 1, headStartingPosition: { x: 2, t: 3 }})).toThrow()
+  it('should not create a snake with an invalid initialHeadPosition - no y', () => {
+    expect(() => makeSnake({ initialSize: 1, initialHeadPosition: { x: 2, t: 3 }})).toThrow()
   });
 
   it('should grow', () => {
-    const sut = makeSnake({ size: 1, headStartingPosition: { x: 0, y: 0 }})
+    const sut = makeSnake({ initialSize: 3, initialHeadPosition: { x: 5, y: 0 }})
     sut.grow()
 
-    expect(sut.getSize()).toBe(2)
+    expect(sut.getSize()).toBe(4)
+  })
+
+
+  it('should add copy of tail at the end of nodes when growing', () => {
+    const sut = makeSnake({ initialSize: 3, initialHeadPosition: { x: 5, y: 0 }})
+    sut.grow()
+
+    expect(sut.getNodes()[3].getX()).toBe(sut.getNodes()[2].getX())
+    expect(sut.getNodes()[3].getY()).toBe(sut.getNodes()[2].getY())
   })
 
   it('should get direction', () => {
-    const sut = makeSnake({ size: 1, headStartingPosition: { x: 0, y: 0 }})
+    const sut = makeSnake({ initialSize: 1, initialHeadPosition: { x: 0, y: 0 }})
 
-    expect(sut.getDirection()).toEqual({x: 0, y: 0})
+    expect(sut.getDirection()).toEqual({ x: 0, y: 0 })
   })
+
+  it('should set direction up', () => {
+    const sut = makeSnake({ initialSize: 1, initialHeadPosition: { x: 0, y: 0 }})
+    sut.lookUp()
+
+    expect(sut.getDirection()).toEqual({ x: 0, y: -1})
+  })
+
+  it('should not set direction up when looking down', () => {
+    const sut = makeSnake({ initialSize: 1, initialHeadPosition: { x: 0, y: 0 }})
+    sut.lookDown()
+    sut.lookUp()
+
+    expect(sut.getDirection()).toEqual({ x: 0, y: 1 })
+  })
+
+  it('should set direction down', () => {
+    const sut = makeSnake({ initialSize: 1, initialHeadPosition: { x: 0, y: 0 }})
+    sut.lookDown()
+
+    expect(sut.getDirection()).toEqual({ x: 0, y: 1 })
+  })
+
+  it('should not set direction down when looking up', () => {
+    const sut = makeSnake({ initialSize: 1, initialHeadPosition: { x: 0, y: 0 }})
+    sut.lookUp()
+    sut.lookDown()
+
+    expect(sut.getDirection()).toEqual({ x: 0, y: -1 })
+  })
+
+  it('should set direction left', () => {
+    const sut = makeSnake({ initialSize: 1, initialHeadPosition: { x: 0, y: 0 }})
+    sut.lookLeft()
+
+    expect(sut.getDirection()).toEqual({ x: -1, y: 0})
+  });
+
+  it('should not set direction left when looking right', () => {
+    const sut = makeSnake({ initialSize: 1, initialHeadPosition: { x: 0, y: 0 }})
+    sut.lookRight()
+    sut.lookLeft()
+
+    expect(sut.getDirection()).toEqual({ x: 1, y: 0})
+  });
+
+  it('should set direction right', () => {
+    const sut = makeSnake({ initialSize: 1, initialHeadPosition: { x: 0, y: 0 }})
+    sut.lookRight()
+
+    expect(sut.getDirection()).toEqual({ x: 1, y: 0})
+  });
+
+  it('should not set direction right when looking left', () => {
+    const sut = makeSnake({ initialSize: 1, initialHeadPosition: { x: 0, y: 0 }})
+    sut.lookLeft()
+    sut.lookRight()
+
+    expect(sut.getDirection()).toEqual({ x: -1, y: 0})
+  });
+
+  it('should initialize nodes with provided initialSize', () => {
+    const sut = makeSnake({ initialSize: 3, initialHeadPosition: { x: 5, y: 0 }})
+
+    expect(sut.getNodes().length).toBe(3)
+  });
+
+  it('should initialize nodes in correct positions', () => {
+    const sut = makeSnake({ initialSize: 3, initialHeadPosition: { x: 5, y: 0 }})
+
+    expect(sut.getNodes()[0].getX()).toBe(5)
+    expect(sut.getNodes()[0].getY()).toBe(0)
+    
+    expect(sut.getNodes()[1].getX()).toBe(4)
+    expect(sut.getNodes()[1].getY()).toBe(0)
+
+    expect(sut.getNodes()[2].getX()).toBe(3)
+    expect(sut.getNodes()[2].getY()).toBe(0)
+  });
 });
