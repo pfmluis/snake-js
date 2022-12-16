6e50d757a16521580bdb650ff633ca8b920de296
Pedro Luís <pedroluis@MacBook-Air-de-Pedro-5.local>
2022-12-16 07:18:30 +0100

    feat: stored apple x and y in apple instance

--- src/app/domain/entities/apple.js
+++ src/app/domain/entities/apple.js
@@ -1,9 +1,16 @@
 export default function buildMakeApple() {
   return function() {
+    let x, y;
+
     return Object.freeze({
+      getX: () => x,
+      getY: () => y,
       placeApple: (cells) => {
         const randomIndex = Math.floor(Math.random() * (cells.length));
-        cells[randomIndex].setHasApple(true)
+        const cell = cells[randomIndex]
+        cell.setHasApple(true)
+        x = cell.getX()
+        y = cell.getY()
       }
     })
   }
--- src/app/domain/entities/apple.spec.js
+++ src/app/domain/entities/apple.spec.js
@@ -9,10 +9,13 @@ describe('Apple', () => {
   it('should place apple in one of the provided cells', () => {
     const sut = makeApple()
 
-    const emptyCells = [makeCell({ x: 0, y: 0}), makeCell({ x: 1, y: 0}), makeCell({ x: 2, y: 0 })]
-    sut.placeApple(emptyCells)
+    const cells = [makeCell({ x: 0, y: 0}), makeCell({ x: 1, y: 0}), makeCell({ x: 2, y: 0 })]
+    sut.placeApple(cells)
 
-    const cell = emptyCells.find((cell) => cell.hasApple())
+    const cell = cells.find((cell) => cell.getX() == sut.getX() && cell.getY() == sut.getY())
     expect(cell).toBeDefined()
+
+    const cellWithApple = cells.find((cell) => cell.hasApple())
+    expect(cellWithApple).toBeDefined()
   });
 })
